2157e5badd53e587d81170b362b851340c183cf1
Niels Thykier <niels@thykier.net>
2011-10-15 00:07:17 +0200

    Added missing file

---
+++ bin/run-single-test.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use BritneyTest;
+
+my ($britney, $TESTSET, $RUNDIR, $test) = @ARGV;
+
+die "Usage: $0 <britney> <testset> <rundir> <test>"
+    unless $britney && $TESTSET && $RUNDIR && $test;
+die "Testset does not exists" unless -d $TESTSET;
+unless ( -d $RUNDIR ) {
+    mkdir $RUNDIR, 0777 or die "mkdir $RUNDIR: $!";
+}
+
+my $bt = BritneyTest->new ({}, "$RUNDIR/$test", "$TESTSET/$test");
+$bt->setup;
+$bt->run ($britney);
+
+exit 0;
+
