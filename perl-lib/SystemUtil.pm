cefa7c27e21e952682f0f21cf18aeb238941d780
Niels Thykier <niels@thykier.net>
2013-05-05 17:56:49 +0200

    SystemUtil: Reset the OOM_ADJ score for new processes
    The processes (e.g. Britney or rsync) are not important and the former
    tends to be somewhat resource hungry.  So ensure OOM_ADJ score is
    reset to something more attractive than "Never kill" (atm. 0).
    
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- perl-lib/SystemUtil.pm
+++ perl-lib/SystemUtil.pm
@@ -10,6 +10,10 @@ use warnings;
 
 use base 'Exporter';
 
+use constant MIN_OOM_ADJ => 0;
+
+use Fcntl qw(SEEK_SET);
+
 our @EXPORT = (qw(system_file));
 
 sub system_file {
@@ -20,9 +24,26 @@ sub system_file {
     die "fork failed: $!" unless defined $pid;
     $ENV{PERLLIB} = join ':',@INC;
     unless ($pid) {
-        # child - re-direct STDERR to STDOUT and exec
+        # child - [Linux] Ensure that the OOM killer considers
+        # us a target in low memory conditions.
+        if ( -e '/proc/self/oom_adj') {
+            open(my $oom_fd, '<', '/proc/self/oom_adj')
+                or die "open oom_adj: $!";
+            my $score = <$oom_fd>;
+            chomp($score);
+            close($fd) or die "close oom_adj: $!";
+            if ($score < MIN_OOM_ADJ) {
+                # Re-open oom_adj (it doesn't like seeking)
+                open($oom_fd,  '>', '/proc/self/oom_adj')
+                    or die "open oom_adj [write]: $!";
+                print {$oom_fd} MIN_OOM_ADJ . "\n";
+                close($oom_fd) or die "close oom_adj [write]: $!";
+            }
+
+        }
+        # re-direct STDERR to STDOUT and exec
         close STDERR;
-        open STDERR, '>&STDOUT' or die "reopen stderr: $!";
+        open(STDERR, '>&', \*STDOUT) or die "reopen stderr: $!";
         exec @$cmd or die "exec @$cmd failed: $!";
     }
 
